b6f9794a546f693fe068e66568b291ea414274f3
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-09-07 20:15:08 +0330

    feat: add app routing structure

---
+++ src/app/router/AppRouter.jsx
@@ -0,0 +1,45 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import LoginPage from "../../features/auth/pages/LoginPage";
+import RegisterPage from "../../features/auth/pages/RegisterPage";
+import Dashboard from "../../components/layout/Dashboard";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Default route */}
+        <Route
+          path="/"
+          element={<Navigate to="/login" replace />}
+        />
+
+        {/* Authentication */}
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+
+        <Route
+          path="/register"
+          element={<RegisterPage />}
+        />
+
+        {/* Dashboard */}
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
